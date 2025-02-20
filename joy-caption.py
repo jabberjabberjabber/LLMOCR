e8ddf8176d1e7213f29aa0f2f2d385e42aab1c38
jabberjabberjabber <75267439+jabberjabberjabber@users.noreply.github.com>
2025-02-20 15:18:45 -0500

    Update joy-caption.py

--- joy-caption.py
+++ joy-caption.py
@@ -58,7 +58,7 @@ class ProcessingThread(QThread):
             for i, file_path in enumerate(self.files):
                 result, output_path = self.processor.process_file(file_path)
                 if result:
-                    #self.processor.save_result(result, output_path)
+                    self.processor.save_result(result, output_path)
                     pass
                 self.progress.emit(i + 1, len(self.files))
             self.finished.emit()
